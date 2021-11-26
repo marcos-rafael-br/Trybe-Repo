dcc61a45095ec3a4d22532f0154da4b2f60c2620
marcos <mrpf10@hotmail.com>
2021-11-25 23:25:47 -0300

    terminar depois

--- fundamentos/5-javascript-dom-eventos-e-web-storage/3-javascript-eventos/Calendário/script.js
+++ fundamentos/5-javascript-dom-eventos-e-web-storage/3-javascript-eventos/Calendário/script.js
@@ -70,14 +70,4 @@ function createButton(buttonName) {
 
 createButton("Feriados");
 
-/* function createHolidayButton(buttonName) {
-  let buttonContainer = document.querySelector('.buttons-container');
-  let newButton = document.createElement('button');
-  let newButtonID = 'btn-holiday';
 
-  newButton.innerHTML = buttonName;
-  newButton.id = newButtonID;
-  buttonContainer.appendChild(newButton);
-};
-
-createHolidayButton('Feriados'); */
