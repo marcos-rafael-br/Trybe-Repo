084753ea1d9c561b3b5cb4801306c320cd02bc0a
marcos <mrpf10@hotmail.com>
2021-12-21 12:50:59 -0300

    exercicio resolvido totalmente da minha forma

--- Pagina personalizavel/script.js
+++ Pagina personalizavel/script.js
@@ -2,15 +2,19 @@ window.onload = function () {
   const body = document.querySelector('body');
   const main = document.querySelector('main');
   const textarea = document.querySelector('#textarea');
+
   const corDeFundo = document.querySelector('#bgcolor');
   const buttonCorDeFundo = document.querySelector('#button-bgcolor');
   const cor = document.querySelector('#color');
   const buttonCor = document.querySelector('#button-color');
   const font = document.querySelector('#font');
   const buttonFont = document.querySelector('#button-font');
-  const buttonSave = document.querySelector('#button-save');
+  const space = document.querySelector('#space');
+  const buttonSpace = document.querySelector('#button-space');
+  const family = document.querySelector('#family');
+  const buttonFamily = document.querySelector('#button-family');
 
-  console.log(main);
+  const buttonSave = document.querySelector('#button-save');
 
   function changeBgColor() {
     let cor = corDeFundo.value;
@@ -23,6 +27,16 @@ window.onload = function () {
   function changeFont() {
     let tamanhoFont = font.value;
     textarea.style.fontSize = `${tamanhoFont}px`;
+  }
+
+  function changeLineHeight() {
+    let espacoFont = space.value;
+    textarea.style.lineHeight = espacoFont;
+  }
+
+  function changeFontFamily() {
+    let familyFont = family.value;
+    textarea.style.fontFamily = familyFont;
   }
 
   function save() {
@@ -42,5 +56,7 @@ window.onload = function () {
   buttonCorDeFundo.addEventListener('click', changeBgColor);
   buttonCor.addEventListener('click', changeColor);
   buttonFont.addEventListener('click', changeFont);
+  buttonSpace.addEventListener('click', changeLineHeight);
+  buttonFamily.addEventListener('click', changeFontFamily);
   buttonSave.addEventListener('click', save);
 };
