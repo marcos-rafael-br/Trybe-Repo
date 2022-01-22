9697032cf9dc578df0624a3b2f7e0c2e6eea52a1
marcos <mrpf10@hotmail.com>
2022-01-22 17:57:36 -0300

    prototipo de site

---
+++ front-end/10-introducao-a-react/1-hello-world-no-react/site/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+/* class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    );
+  }
+} */
+
+export default App;
