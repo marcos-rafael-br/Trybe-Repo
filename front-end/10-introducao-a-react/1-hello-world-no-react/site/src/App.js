30517a15f2eae54d5693af598abbc9381f6fd360
marcos <mrpf10@hotmail.com>
2022-01-22 23:16:19 -0300

    exercicios finalizados

--- front-end/10-introducao-a-react/1-hello-world-no-react/site/src/App.js
+++ front-end/10-introducao-a-react/1-hello-world-no-react/site/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './Header';
+import Cabeça from './Header';
 import Content from './Content';
 import Footer from './Footer';
 
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <Header />
+      <Cabeça />
       <Content />
       <Footer />
     </div>
--- front-end/10-introducao-a-react/1-hello-world-no-react/site/src/Header.jsx
+++ front-end/10-introducao-a-react/1-hello-world-no-react/site/src/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Header extends React.Component {
+class Cabeça extends React.Component {
   render() {
     return (
       <div>
@@ -10,4 +10,4 @@ class Header extends React.Component {
   }
 }
 
-export default Header;
+export default Cabeça;
